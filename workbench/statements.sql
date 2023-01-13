00f9166621e6c7268bf6bd5d6527b2422180aac1
RamiroMeza <rmeza.laba@solvd.com>
2023-01-13 09:39:01 -0300

    equipment that has never been repaired

--- workbench/statements.sql
+++ workbench/statements.sql
@@ -78,8 +78,8 @@ SELECT l.*, r.name FROM labs l RIGHT OUTER JOIN researchs r ON l.id=r.Labs_id;
 SELECT e.id, e.name, h.id FROM equipment e JOIN equipment_suport_history h ON e.id=h.Equipment_id;
 SELECT e.id, e.name, h.id FROM equipment e RIGHT JOIN equipment_suport_history h ON e.id=h.Equipment_id;
 SELECT e.id, e.name, h.id FROM equipment e LEFT JOIN equipment_suport_history h ON e.id=h.Equipment_id;
-SELECT e.id, e.name, h.id FROM equipment e RIGHT OUTER JOIN equipment_suport_history h ON e.id=h.Equipment_id WHERE h.id=null;
-SELECT e.id, e.name FROM equipment e LEFT JOIN equipment_suport_history h ON e.id=h.Equipment_id;
+SELECT e.id, e.name FROM equipment e WHERE NOT EXISTS (SELECT 1 FROM equipment_suport_history h 
+WHERE e.id=h.Equipment_id); 
 
 
  
